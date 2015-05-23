5084976adf6c3083a4c3061bfeaf9582e2737324
Chris Pennello <git@chrispennello.com>
2015-05-23 14:37:31 -0700

    Adds special cases to absolute value functions.
    Adds documentation.

--- math.go
+++ math.go
@@ -7,12 +7,20 @@ import (
 )
 
 // Abs32 returns the absolute value of a 32-bit float.
+//
+// Special cases are:
+//	Abs32(±Inf) = Inf
+//	Abs32(NaN) = NaN
 func Abs32(x float32) float32 {
 	xb := math.Float32bits(x)
 	return math.Float32frombits(xb & 0x7fffffff)
 }
 
 // Abs64 returns the absolute value of a 64-bit float.
+//
+// Special cases are:
+//	Abs64(±Inf) = Inf
+//	Abs64(NaN) = NaN
 func Abs64(x float64) float64 {
 	xb := math.Float64bits(x)
 	return math.Float64frombits(xb & 0x7fffffffffffffff)
