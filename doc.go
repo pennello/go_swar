0aa6fe2ae84723cd96bd88ae265bc2c02ad64ed2
Chris Pennello <git@chrispennello.com>
2015-05-23 12:05:24 -0700

    Adds package GoDoc.

---
+++ doc.go
@@ -0,0 +1,11 @@
+// chris 052315
+
+// Package swar implements many SIMD-within-a-resgister algorithms.
+//
+// These functions may seem to implement fairly trivial logic, but the
+// fun is in taking a look at the implementations.
+//
+// Many of the algorithms are lovingly taken from The Aggregate's MAGIC
+// Algorithms:
+// http://aggregate.org/MAGIC/
+package swar
