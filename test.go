e58104dd3b12097c69d6caa8081cfb0eb144b823
Chris Pennello <git@chrispennello.com>
2015-05-23 16:29:58 -0700

    Adds commenary outlining testing strategy.

--- test.go
+++ test.go
@@ -2,6 +2,14 @@
 
 package swar
 
+// Frequently, a complaint about testing is that you're implementing
+// your logic twice.  In the case of these swar algorithms, though,
+// that's exactly what we want to do!  These algorithms implement some
+// more trivial logic in a weird, interesting, or efficient way.
+// Therefore, our approach is to implement reference functions that
+// implement the same logic in a more normal way, and then just check
+// the two against each other.
+
 import (
 	"os"
 	"testing"
